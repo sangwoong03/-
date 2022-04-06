993e440221b8a275754eea4bc98a8d0bc028f73f
sangwoong <sangwoong03@gmail.com>
2022-04-06 11:34:12 +0900

    Permutation Python_Recursive Statement

--- 수학기본이론/순열_재귀문.py
+++ 수학기본이론/순열_재귀문.py
@@ -10,3 +10,19 @@ print(' '.join(arr))  # 리스트를 문자열로 변환
 
 
 #===================================================#
+# 배열의 원소로 새로운 순열 반환
+# 참조: https://cotak.tistory.com/70
+def permutaition(arr, n):
+    result = []
+
+    if n == 0:
+        return [[]]
+
+    for i, elem in enumerate(arr):
+        for P in permutaition(arr[:i] + arr[i+1:], n-1):
+            result += [[elem]+P]
+
+    return result
+
+
+print(permutaition(arr, 3))
