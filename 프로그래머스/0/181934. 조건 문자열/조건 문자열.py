5bb141f1fecfe9f68e10346c5b977dbf3af0b668
Sangwoong <sangwoong03@gmail.com>
2024-02-22 16:40:01 +0900

    [level 0] Title: 조건 문자열, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181934. 조건 문자열/조건 문자열.py
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.py
@@ -1,5 +1,2 @@
 def solution(ineq, eq, n, m):
-    newEq = '' if eq == '!' else eq
-    answer = 1 if eval(f"{n} {ineq}{newEq} {m}") else 0
-    
-    return answer
+    return int(eval(f"{n} {ineq}{eq.replace('!', '')} {m}"))
