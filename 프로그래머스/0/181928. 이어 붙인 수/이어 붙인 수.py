7f8f8b749f37cfac3f03fbc81ba0326e472c8bd3
Sangwoong <sangwoong03@gmail.com>
2024-02-29 09:29:27 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.py
@@ -0,0 +1,11 @@
+def solution(num_list):
+    odd_sum = ''
+    even_sum = ''
+
+    for num in num_list:
+        if num % 2 == 1:
+            odd_sum += str(num)
+        else:
+            even_sum += str(num)
+
+    return int(odd_sum) + int(even_sum)
