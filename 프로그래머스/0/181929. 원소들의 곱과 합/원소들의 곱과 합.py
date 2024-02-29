e5faf2373c5cff76bc7b38be1151120857f5ebde
Sangwoong <sangwoong03@gmail.com>
2024-02-29 09:23:10 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.py
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.py
@@ -1,11 +1,5 @@
 def solution(num_list):
-    answer_1 = 1
-    answer_2 = 0
-    
-    for i in range(len(num_list)):
-        answer_1 = answer_1 * int(num_list[i])
-    
-    for i in range(len(num_list)):
-        answer_2 += int(num_list[i])
-    
-    return 0 if answer_1 > answer_2 ** 2 else 1
+    answer1 = 1 
+    for i in num_list:
+        answer1 *= i
+    return int(answer1 < sum(num_list) ** 2)
