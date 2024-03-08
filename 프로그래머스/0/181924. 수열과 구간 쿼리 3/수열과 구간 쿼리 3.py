631ef8ea03ae9d58e347a83ef7ef60a5228f2784
Sangwoong <sangwoong03@gmail.com>
2024-03-08 16:36:31 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.07 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
@@ -1,6 +1,4 @@
 def solution(arr, queries):
     for elem in queries:
-        first_value = arr[elem[0]]
-        arr[elem[0]] = arr[elem[1]]
-        arr[elem[1]] = first_value
+        arr[elem[0]],arr[elem[1]] = arr[elem[1]],arr[elem[0]]        
     return arr
