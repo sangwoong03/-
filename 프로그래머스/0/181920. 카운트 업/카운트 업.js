e36a1c7e2f1c4df94a2acc29833b4d656d92b2f0
Sangwoong <sangwoong03@gmail.com>
2024-05-09 14:26:18 +0900

    [level 0] Title: 카운트 업, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.js
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.js
@@ -1,7 +1,7 @@
-function solution(start_num, end_num) {
+function solution(start, end) {
     const answer = [];
     
-    for ( let i = start_num; i <= end_num; i++ ) {
+    for ( let i = start; i <= end; i++ ) {
         answer.push(i)
     }
     return answer;
