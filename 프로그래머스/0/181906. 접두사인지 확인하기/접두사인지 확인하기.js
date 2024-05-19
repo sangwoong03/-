18450c0a567b16dffce613367e8fb8707daa21cc
Sangwoong <sangwoong03@gmail.com>
2024-05-19 14:51:54 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.js
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.js
@@ -1,7 +1,11 @@
-function solution(my_string, is_prefix) {
-    const prefix = [];
-    for (let i = 0; i < my_string.length; i++) {
-        prefix.push(my_string.slice(0, i))
-    }
-    return prefix.includes(is_prefix) ? 1 : 0;
+// function solution(my_string, is_prefix) {
+//     const prefix = [];
+//     for (let i = 0; i < my_string.length; i++) {
+//         prefix.push(my_string.slice(0, i))
+//     }
+//     return prefix.includes(is_prefix) ? 1 : 0;
+// }
+
+const solution = (str, prefix) => {
+    return str.startsWith(prefix) ? 1 : 0;
 }
