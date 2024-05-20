37b2c65f30510e5c616f4c823c08edcfb8e5d3a2
Sangwoong <sangwoong03@gmail.com>
2024-05-20 23:27:57 +0900

    [level 0] Title: 배열 만들기 1, Time: 0.09 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.js
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.js
@@ -1,10 +1,8 @@
 function solution(n, k) {
     let answer = [];
     
-    for (let i = 1; i <= n; i++) {
-        if ( i % k === 0 ) {
-            answer.push(i)
-        }
+    for (let i = k; i <= n; i += k) {
+        answer.push(i)
     }
     return answer;
 }
