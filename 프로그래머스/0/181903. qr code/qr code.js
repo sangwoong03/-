58c32e7c4bbd6f4cb026441040ab0dec80042ef5
Sangwoong <sangwoong03@gmail.com>
2024-05-20 23:07:00 +0900

    [level 0] Title: qr code, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181903. qr code/qr code.js
+++ 프로그래머스/0/181903. qr code/qr code.js
@@ -1,7 +1,11 @@
-function solution(q, r, code) {
-    let answer = '';
-    for (const idx in code) {
-        if (idx % q === r) answer += code[idx]
-    }
-    return answer;
+// function solution(q, r, code) {
+//     let answer = '';
+//     for (const idx in code) {
+//         if (idx % q === r) answer += code[idx]
+//     }
+//     return answer;
+// }
+
+const solution = (q, r, code) => {
+    return [...code].filter((_elem, index) => index % q === r).join('')
 }
