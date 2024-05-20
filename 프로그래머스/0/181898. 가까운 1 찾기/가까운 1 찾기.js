2623778c075dcfbbba411e0a28ccf6fea776e3bc
Sangwoong <sangwoong03@gmail.com>
2024-05-20 23:50:17 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.js
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.js
@@ -1,4 +1,6 @@
-function solution(arr, idx) {
-    const res = arr.slice(idx).indexOf(1);
-    return res > -1 ? res + idx : res;
-}
+// function solution(arr, idx) {
+//     const res = arr.slice(idx).indexOf(1);
+//     return res > -1 ? res + idx : res;
+// }
+
+const solution = (a,i) => a.indexOf(1, i);
