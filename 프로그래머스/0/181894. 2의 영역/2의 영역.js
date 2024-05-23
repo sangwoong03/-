bb0768dd50243fb4b43e658f6e05ba63808737d1
Sangwoong <sangwoong03@gmail.com>
2024-05-23 22:51:24 +0900

    [level 0] Title: 2의 영역, Time: 0.27 ms, Memory: 41.4 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.js
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.js
@@ -1,11 +1,19 @@
-function solution(arr) { 
-    const index = [];
-    for (const i in arr) {
-        if (arr[i] === 2) index.push(i)
-    }
+// function solution(arr) { 
+//     const index = [];
+//     for (const i in arr) {
+//         if (arr[i] === 2) index.push(i)
+//     }
     
-    if (index.length === 0) return [-1];
-    if (index.length === 1) return [arr[index[0]]];
+//     if (index.length === 0) return [-1];
+//     if (index.length === 1) return [arr[index[0]]];
 
-    return arr.slice(index[0], +index[index.length - 1] + 1)
+//     return arr.slice(index[0], +index[index.length - 1] + 1)
+// }
+
+
+const solution = (arr) => {
+    const startIdx = arr.indexOf(2);
+    const endIdx = arr.lastIndexOf(2);
+
+    return startIdx === -1 ? [-1] : arr.slice(startIdx, endIdx + 1);
 }
