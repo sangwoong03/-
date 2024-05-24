f2ac4191e0eea0072dce7cc7c882457f2e2a9904
Sangwoong <sangwoong03@gmail.com>
2024-05-25 08:57:02 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 31.33 ms, Memory: 44.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
@@ -0,0 +1,11 @@
+function solution(arr) {
+    return arr.map(elem =>  {
+        if (elem >= 50 && elem % 2 === 0) {
+            return elem / 2
+        }
+        if (elem < 50 && elem % 2 !== 0) {
+            return elem * 2
+        }
+        return elem
+    });
+}
