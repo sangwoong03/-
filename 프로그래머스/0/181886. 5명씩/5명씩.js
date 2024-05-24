43a06c4d3fd2faaa1e792d327491acc42160edd1
Sangwoong <sangwoong03@gmail.com>
2024-05-24 17:07:36 +0900

    [level 0] Title: 5명씩, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181886. 5명씩/5명씩.js
+++ 프로그래머스/0/181886. 5명씩/5명씩.js
@@ -1,7 +1,11 @@
-function solution(names) {
-    const answer = [];
-    for (let i = 0; i < names.length; i += 5 ) {
-        answer.push(names[i])
-    }
-    return answer;
+// function solution(names) {
+//     const answer = [];
+//     for (let i = 0; i < names.length; i += 5 ) {
+//         answer.push(names[i])
+//     }
+//     return answer;
+// }
+
+const solution = (names) => {
+    return names.filter((_, index) => index % 5 === 0);
 }
