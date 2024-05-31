009f1ad87162f0c6c0c4e2d69e5b8b1efef0c2ae
Sangwoong <sangwoong03@gmail.com>
2024-05-31 16:38:23 +0900

    [level 0] Title: A 강조하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.js
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.js
@@ -1,9 +1,13 @@
+// function solution(myString) {
+//     return [...myString].map(char => {
+//         if (char === 'a' || char === 'A') {
+//             return char.toUpperCase()
+//         } else {
+//             return char.toLowerCase()
+//         }
+//     }).join('');
+// }
+
 function solution(myString) {
-    return [...myString].map(char => {
-        if (char === 'a' || char === 'A') {
-            return char.toUpperCase()
-        } else {
-            return char.toLowerCase()
-        }
-    }).join('');
+    return myString.toLowerCase().replace(/a/g, 'A')
 }
