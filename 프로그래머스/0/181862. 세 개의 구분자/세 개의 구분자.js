e847e7c6e0eb2be2c98ca4b1bd81c3add10c5ffa
Sangwoong <sangwoong03@gmail.com>
2024-06-03 14:36:13 +0900

    [level 0] Title: 세 개의 구분자, Time: 17.74 ms, Memory: 53 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.js
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.js
@@ -3,21 +3,26 @@
 * myStr 내 seperator가 1개 이상이면, 잘라서 배열에 넣어 반환
 */
 
-function solution(myStr) {
-    const seperator = ['a', 'b', 'c']
+// function solution(myStr) {
+//     const seperator = ['a', 'b', 'c']
     
-    let newStr = ''
-    const answer = []
-    for (let i = 0; i < myStr.length; i++) {
-        const char = myStr[i]
-        if (!seperator.includes(char)) {
-            newStr += char
-        }
+//     let newStr = ''
+//     const answer = []
+//     for (let i = 0; i < myStr.length; i++) {
+//         const char = myStr[i]
+//         if (!seperator.includes(char)) {
+//             newStr += char
+//         }
         
-        if ((seperator.includes(char) || i === myStr.length - 1) && newStr) {
-          answer.push(newStr)
-          newStr = ''
-        }
-    }
-    return answer.length > 0 ? answer : ['EMPTY']
+//         if ((seperator.includes(char) || i === myStr.length - 1) && newStr) {
+//           answer.push(newStr)
+//           newStr = ''
+//         }
+//     }
+//     return answer.length > 0 ? answer : ['EMPTY']
+// }
+
+function solution(myStr) {
+    const list = myStr.split(/[a|b|c]/).filter(str => str);
+    return list.length ? list : ["EMPTY"];
 }
