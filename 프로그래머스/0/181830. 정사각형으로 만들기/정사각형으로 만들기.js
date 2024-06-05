d83e3027dcd4e6dd9efceca537c5ddac40fc307a
Sangwoong <sangwoong03@gmail.com>
2024-06-05 16:30:20 +0900

    [level 0] Title: 정사각형으로 만들기, Time: 0.06 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.js
+++ 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.js
@@ -3,14 +3,14 @@ const solution = (arr) => {
     const numCols = arr[0].length;
 
     if (numRows > numCols) {
-        // Add columns
         for (let i = 0; i < numRows; i++) {
             while (arr[i].length < numRows) {
                 arr[i].push(0);
             }
         }
-    } else if (numCols > numRows) {
-        // Add rows
+    }
+    
+    if (numCols > numRows) {
         while (arr.length < numCols) {
             arr.push(new Array(numCols).fill(0));
         }
