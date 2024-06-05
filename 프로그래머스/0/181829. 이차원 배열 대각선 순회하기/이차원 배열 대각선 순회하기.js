a9c7750670041d29e4231eecfc176eceefdee1fa
Sangwoong <sangwoong03@gmail.com>
2024-06-05 16:02:48 +0900

    [level 0] Title: 이차원 배열 대각선 순회하기, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181829. 이차원 배열 대각선 순회하기/이차원 배열 대각선 순회하기.js
+++ 프로그래머스/0/181829. 이차원 배열 대각선 순회하기/이차원 배열 대각선 순회하기.js
@@ -1,26 +1,11 @@
-// const solution = (arr, k) => {
-//   let result = 0;
-//   for (let i = 0; i < arr.length; i++) {
-//     for (let j = 0; j < arr.length; j++){
-//       if (i + j <= k) {
-//         result += arr[i][j]
-//       }
-//     }
-//   }
-//   return result
-// }
-function solution(board, k) {
-    let totalSum = 0;
-    let rows = board.length;
-    let cols = board[0].length;
-
-    for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < cols; j++) {
-            if (i + j <= k) {
-                totalSum += board[i][j];
-            }
-        }
+const solution = (arr, k) => {
+  let result = 0;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++){
+      if (i + j <= k) {
+        result += arr[i][j]
+      }
     }
-
-    return totalSum;
+  }
+  return result
 }
