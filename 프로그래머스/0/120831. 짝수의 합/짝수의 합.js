fbfe112dca492ed72c059f362fff82e38fe9dfc9
Sangwoong <sangwoong03@gmail.com>
2024-06-10 10:41:13 +0900

    [level 0] Title: 짝수의 합, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.js
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.js
@@ -1,7 +1,9 @@
 const solution = (n) => {
     let result = 0;
-    for (let i = 0; i <= n; i++) {
-        if (i % 2 === 0) result += i;
+    
+    for(let i=2 ; i<=n ; i+=2) {
+        result += i;
     }
-    return result
+    
+    return result;
 }
