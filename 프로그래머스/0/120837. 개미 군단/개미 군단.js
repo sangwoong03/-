e4d3e842c7e869b98c6d244ba09a2a82f44b7eb4
Sangwoong <sangwoong03@gmail.com>
2024-06-11 09:58:06 +0900

    [level 0] Title: 개미 군단, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.js
@@ -1,11 +1,11 @@
 const solution = (hp) => {
-    const general = Math.floor(hp / 5);
-    const _general = hp % 5;
+    const generals = Math.floor(hp / 5);
+    hp %= 5;
     
-    const soldier = _general > 0 ? Math.floor(_general / 3) : 0;
-    const _soldier = _general % 3;
-  
-    const ergate = (_soldier > 0) ? Math.floor(_soldier / 1) : 0;
-  
-    return general + soldier + ergate;
+    const soldiers = Math.floor(hp / 3);
+    hp %= 3;
+    
+    const ergates = hp;
+
+    return generals + soldiers + ergates;
 }
