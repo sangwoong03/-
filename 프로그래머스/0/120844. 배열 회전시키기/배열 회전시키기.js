3427b60e805e88181016632b82fa65c2aa909775
Sangwoong <sangwoong03@gmail.com>
2024-06-13 17:09:01 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.js
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -1,23 +1,37 @@
-function solution(numbers, direction) {
-    const answer = [];
+// function solution(numbers, direction) {
+//     const answer = [];
     
-    if (direction === 'right') {
-        for (let i = 0; i < numbers.length; i++) {
-            if (i === 0) {
-                answer[i] = numbers[numbers.length - 1];
-            } else {
-                answer[i] = numbers[i-1];
-            }
-        }
+//     if (direction === 'right') {
+//         for (let i = 0; i < numbers.length; i++) {
+//             if (i === 0) {
+//                 answer[i] = numbers[numbers.length - 1];
+//             } else {
+//                 answer[i] = numbers[i-1];
+//             }
+//         }
+//     } else {
+//         for (let i = 0; i < numbers.length; i++) {
+//             if (i+1 ===numbers.length){
+//                 answer[i] = numbers[0];
+//             }
+//             else {
+//                 answer[i] = numbers[i+1];
+//             }
+//         }
+//     }
+//     return answer;
+// }
+
+const solution = (numbers, direction) => {
+    let answer = [];
+
+    if ("right" == direction) {
+        numbers.unshift(numbers.pop());
     } else {
-        for (let i = 0; i < numbers.length; i++) {
-            if (i+1 ===numbers.length){
-                answer[i] = numbers[0];
-            }
-            else {
-                answer[i] = numbers[i+1];
-            }
-        }
+        numbers.push(numbers.shift());
     }
+
+    answer = numbers;
+
     return answer;
 }
