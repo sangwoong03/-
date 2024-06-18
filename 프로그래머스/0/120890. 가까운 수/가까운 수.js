6380dce476d9f0dea5335bcb53737ac4f8234b7b
Sangwoong <sangwoong03@gmail.com>
2024-06-18 10:31:49 +0900

    [level 0] Title: 가까운 수, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.js
@@ -12,3 +12,11 @@ const solution = (array, n) => {
 
     return answer;
 };
+
+/* 
+function solution(array, n) {
+    array.sort((a,b) => Math.abs(n - a) - Math.abs(n - b) || a - b);
+
+    return array[0];
+}
+*/
