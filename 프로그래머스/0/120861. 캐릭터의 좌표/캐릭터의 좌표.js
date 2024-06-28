d5259aeffe2aa2b84a620f590b8b6c4139c6025e
Sangwoong <sangwoong03@gmail.com>
2024-06-28 17:52:57 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -1,36 +1,25 @@
 function solution(keyinput, board) {
-    // 초기 좌표를 [0, 0]으로 설정
-    let position = [0, 0];
+    const direction = {
+        right: [1, 0],
+        left: [-1, 0],
+        up: [0, 1],
+        down: [0, -1]
+    };
     
-    // board의 크기를 이용하여 이동할 수 있는 최대 범위 계산
     const xLimit = Math.floor(board[0] / 2);
     const yLimit = Math.floor(board[1] / 2);
     
-    // 방향키 입력을 처리
-    for (let key of keyinput) {
-        switch (key) {
-            case 'up':
-                if (position[1] < yLimit) {
-                    position[1] += 1;
-                }
-                break;
-            case 'down':
-                if (position[1] > -yLimit) {
-                    position[1] -= 1;
-                }
-                break;
-            case 'left':
-                if (position[0] > -xLimit) {
-                    position[0] -= 1;
-                }
-                break;
-            case 'right':
-                if (position[0] < xLimit) {
-                    position[0] += 1;
-                }
-                break;
-        }
+    let coordination = [0, 0];
+    
+    for (const key of keyinput) {
+        const [dX, dY] = direction[key];
+        const newX = coordination[0] + dX;
+        const newY = coordination[1] + dY;
+        
+        if (Math.abs(newX) <= xLimit) coordination[0] = newX;
+        if (Math.abs(newY) <= yLimit) coordination[1] = newY;
+        
     }
     
-    return position;
+    return coordination;
 }
